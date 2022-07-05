958203a67431b45c5449452d1451f41db342d44a
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-07-05 19:14:14 -0300

    modificando menu provisoriamente

--- beerStockapp/src/app/shared/footer/footer.component.ts
+++ beerStockapp/src/app/shared/footer/footer.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  anio: number = new Date().getFullYear(); //muestra el año actualizado
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

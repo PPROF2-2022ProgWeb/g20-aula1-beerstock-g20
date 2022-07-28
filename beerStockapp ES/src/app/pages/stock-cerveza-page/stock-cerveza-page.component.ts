605bc03e991f4c4e397cc1c4ed3f979e224e221b
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-07-28 19:23:22 -0300

    Modificando directorios

---
+++ beerStockapp ES/src/app/pages/stock-cerveza-page/stock-cerveza-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'stock',
+  templateUrl: './stock-cerveza-page.component.html',
+  styleUrls: ['./stock-cerveza-page.component.css'],
+})
+export class StockCervezaPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
